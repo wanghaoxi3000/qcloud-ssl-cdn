174c07ce99e69907faf2149c9a6ce91e53b760a8
Dorad <23170065+Doradx@users.noreply.github.com>
2024-10-10 04:21:31 +0800

    add ZONE_ID. (#16)
    * add ZONE_ID.
    
    * default value for ZONE_ID
    
    ---------
    
    Co-authored-by: Fubin Zhang <zfb132@gmail.com>

--- docker/update.sh
+++ docker/update.sh
@@ -77,7 +77,7 @@ PURGE_URL = True
 URLS_FILE = "urls.txt"
 # 仅用于边缘安全加速平台EO更换SSL证书，不用于CDN
 # 区域ID：可以手动利用函数get_teo_zones_list获取所有的加速区域ID；格式为 zone-xxxxxx
-ZONE_ID = ""
+ZONE_ID = "${ZONE_ID:-}"
 EOF
 # 更新CDN证书
 cd ${WORK_DIR} && python main.py
