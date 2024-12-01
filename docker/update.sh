b783903c25f7a0d843a0c823b360253faec2e41c
Nan Mu <121783294+aa12110@users.noreply.github.com>
2024-12-01 21:25:15 +0800

    fix bug: override existing old certificates

--- docker/update.sh
+++ docker/update.sh
@@ -7,7 +7,7 @@ if [ "${ACME_ENABLED:=true}" = "true" ]; then
   ${ACME_HOME}/acme.sh ${ACME_PARAMS:-} --force --issue --cert-home ${CERT_HOME} -d ${ACME_DOMAIN} -d *.${ACME_DOMAIN} --dns ${ACME_DNS_TYPE}
   # 兼容ecc证书
   if [ -d "${CERT_HOME}/${ACME_DOMAIN}_ecc" ]; then
-    cp -r ${CERT_HOME}/${ACME_DOMAIN}_ecc ${CERT_HOME}/${ACME_DOMAIN}
+    cp -rf ${CERT_HOME}/${ACME_DOMAIN}_ecc ${CERT_HOME}/${ACME_DOMAIN}
     echo "The ECC certificate is detected"
   fi
 fi
